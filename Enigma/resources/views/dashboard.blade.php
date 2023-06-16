7af7228a717fb4acbe0fa648c1702c8555de6518
Stijn ter haar <stijn.ter.haar2004@gmail.com>
2023-06-16 16:10:09 +0200

    Dashboard update

--- Enigma/resources/views/dashboard.blade.php
+++ Enigma/resources/views/dashboard.blade.php
@@ -4,35 +4,21 @@
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
+
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex">
-            <!-- Left Side Menu -->
-            <div class="w-1/4 pr-4">
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg ">
-                    <div class="p-4 left_row_users">
-                        <div class="text-lg font-semibold mb-4">Chat</div>
-                        <!-- Search bar here -->
-                        <div class="flex items-center space-x-2 mb-2">
-                            <div class="text-gray-800 dark:text-gray-10 w-full">
-                                <input type="text" id="search" placeholder="Search users" />
-                            </div>
-                        </div>
-                        <!-- Display the users -->
-                        <div class="flex search-result items-start  mb-2 flex-col">
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex flex-col-reverse sm:flex-row">
             <!-- Chat Area -->
-            <div class="w-3/4 pl-4">
+            <div class="w-full sm:w-3/4 pl-4">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6">
+                        <!-- Username -->
+                        <div class="mb-4 text-lg font-semibold text-gray-800 dark:text-gray-100 pb-4">John Doe</div>
+
                         <div class="flex flex-col space-y-4 h-96 overflow-y-auto">
                             <div class="flex items-start">
                                 <div class="rounded-full bg-gray-300 w-10 h-10 flex-shrink-0"></div>
                                 <div class="ml-4 p-4 rounded-lg bg-gray-200 dark:bg-gray-700">
+                                    <div class="text-xs text-gray-500">John Doe</div>
                                     <div class="text-sm text-gray-800 dark:text-gray-100">Hello! How can I assist you today?</div>
                                 </div>
                             </div>
@@ -47,9 +33,11 @@
                             <div class="flex items-start">
                                 <div class="rounded-full bg-gray-300 w-10 h-10 flex-shrink-0"></div>
                                 <div class="ml-4 p-4 rounded-lg bg-gray-200 dark:bg-gray-700">
-                                    <div class="text-sm text-gray-800 dark:text-gray-100">Can you assist me with setting up my account?</div>
+                                    <div class="text-xs text-gray-500">John Doe</div>
+                                    <div class="text-sm text-gray-800 dark:text-gray-100">Can you assist me with setting up my account?</div> <!-- Chat message -->
                                 </div>
                             </div>
+
 
                             <!-- Add more chat bubbles as needed -->
 
@@ -71,64 +59,90 @@
                     </div>
                 </div>
             </div>
+
+            <!-- Left Side Menu -->
+            <div class="w-full sm:w-1/4 pl-4 pb-4">
+                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-4 left_row_users">
+                        <div class="text-lg font-semibold mb-4 text-gray-800 dark:text-gray-100">Chat</div>
+                        <!-- Search bar here -->
+                        <div class="flex items-center space-x-2 mb-2">
+                            <div class="text-gray-800 dark:text-gray-10 w-full">
+                                <input class="w-full" type="text" id="search" placeholder="Search users" />
+                            </div>
+                        </div>
+                        <!-- Display the users -->
+                        <div class="flex search-result items-start mb-2 flex-col">
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
+<<<<<<< Updated upstream
+=======
+
+
+
+>>>>>>> Stashed changes
     <!-- Scripts -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script>
         $(document).ready(function() {
-    var searchInput = $('#search');
-    var userContainer = $('.search-result');
-    var searchBar = $('.search-bar');
-    var previousResults = [];
+            var searchInput = $('#search');
+            var userContainer = $('.search-result');
+            var searchBar = $('.search-bar');
+            var previousResults = [];
 
-    searchInput.on('input', function() {
-        var query = searchInput.val();
+            searchInput.on('input', function() {
+                var query = searchInput.val();
 
-        if (query.length > 0) {
-            $.ajax({
-                url: '/search',
-                type: 'GET',
-                data: {query: query},
-                success: function(response) {
-                    var users = response.users;
+                if (query.length > 0) {
+                    $.ajax({
+                        url: '/search',
+                        type: 'GET',
+                        data: {
+                            query: query
+                        },
+                        success: function(response) {
+                            var users = response.users;
 
+                            userContainer.empty();
+
+                            if (users.length > 0) {
+                                users.forEach(function(user) {
+                                    var userElement = '<a href="/dashboard/' + user.id + '">' +
+                                        '<div class="flex items-center space-x-2 mb-2">' +
+                                        '<div class="rounded-full bg-blue-500 w-10 h-10 flex-shrink-0"></div>' +
+                                        '<div class="text-gray-800 dark:text-gray-100">' + user.name + '</div>' +
+                                        '</div>' +
+                                        '</a>';
+
+                                    userContainer.append(userElement);
+
+                                    previousResults.push(user);
+                                });
+                            } else {
+                                var noUsersElement = '<div class="text-gray-800 dark:text-gray-100">No users found</div>';
+                                userContainer.append(noUsersElement);
+                            }
+                        },
+                        error: function(xhr, status, error) {
+                            console.error('Search error:', error);
+                        }
+                    });
+                } else {
                     userContainer.empty();
-
-                    if (users.length > 0) {
-                        users.forEach(function(user) {
-                            var userElement = '<a href="/dashboard/' + user.id + '">' +
-                                                '<div class="flex items-center space-x-2 mb-2">' +
-                                                    '<div class="rounded-full bg-blue-500 w-10 h-10 flex-shrink-0"></div>' +
-                                                    '<div class="text-gray-800 dark:text-gray-100">' + user.name + '</div>' +
-                                                '</div>' +
-                                            '</a>';
-
-                            userContainer.append(userElement);
-
-                            previousResults.push(user);
-                        });
-                    } else {
-                        var noUsersElement = '<div class="text-gray-800 dark:text-gray-100">No users found</div>';
-                        userContainer.append(noUsersElement);
-                    }
-                },
-                error: function(xhr, status, error) {
-                    console.error('Search error:', error);
                 }
             });
-        } else {
-            userContainer.empty();
-        }
-    });
 
-    searchInput.on('focus', function() {
-        searchBar.addClass('active');
-    });
+            searchInput.on('focus', function() {
+                searchBar.addClass('active');
+            });
 
-    searchInput.on('blur', function() {
-        searchBar.removeClass('active');
-    });
-});
+            searchInput.on('blur', function() {
+                searchBar.removeClass('active');
+            });
+        });
     </script>
 </x-app-layout>
