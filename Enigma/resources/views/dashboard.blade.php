d0f703d0dc0e63ec1890f3973c0d92e7d6271c76
JulianW051 <julianwessels1@gmail.com>
2023-06-23 14:09:12 +0200

    chat openen

--- Enigma/resources/views/dashboard.blade.php
+++ Enigma/resources/views/dashboard.blade.php
@@ -1,96 +1,62 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                {{ __('Dashboard') }}
+            </h2>
+        </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex flex-col-reverse sm:flex-row">
-            <!-- Chat Area -->
-            <div class="w-full sm:w-3/4 pl-4">
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6">
-                        <!-- Username -->
-                        <div class="mb-4 text-lg font-semibold text-gray-800 dark:text-gray-100 pb-4">John Doe</div>
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex flex-col-reverse sm:flex-row">
 
-                        <div class="flex flex-col space-y-4 h-96 overflow-y-auto">
-                            <div class="flex items-start">
-                                <div class="rounded-full bg-gray-300 w-10 h-10 flex-shrink-0"></div>
-                                <div class="ml-4 p-4 rounded-lg bg-gray-200 dark:bg-gray-700">
-                                    <div class="text-xs text-gray-500">John Doe</div>
-                                    <div class="text-sm text-gray-800 dark:text-gray-100">Hello! How can I assist you today?</div>
-                                </div>
-                            </div>
+    <!-- Chat Area -->
+    <div class="w-full sm:w-3/4 pl-4" id="chat-area" style="display: none;">
+        <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6">
+                <!-- Username -->
+                <div id="chat-username" class="mb-4 text-lg font-semibold text-gray-800 dark:text-gray-100 pb-4"></div>
 
-                            <div class="flex items-end justify-end">
-                                <div class="mr-4 p-4 rounded-lg bg-blue-500 text-white dark:bg-blue-800">
-                                    <div class="text-sm">Sure! I'm here to help.</div>
-                                </div>
-                                <div class="rounded-full bg-gray-300 w-10 h-10 flex-shrink-0"></div>
-                            </div>
-
-                            <div class="flex items-start">
-                                <div class="rounded-full bg-gray-300 w-10 h-10 flex-shrink-0"></div>
-                                <div class="ml-4 p-4 rounded-lg bg-gray-200 dark:bg-gray-700">
-                                    <div class="text-xs text-gray-500">John Doe</div>
-                                    <div class="text-sm text-gray-800 dark:text-gray-100">Can you assist me with setting up my account?</div> <!-- Chat message -->
-                                </div>
-                            </div>
-
-
-                            <!-- Add more chat bubbles as needed -->
-
-                            <div class="flex items-end justify-end">
-                                <div class="mr-4 p-4 rounded-lg bg-blue-500 text-white dark:bg-blue-800">
-                                    <div class="text-sm">Absolutely! I'll guide you through the process.</div>
-                                </div>
-                                <div class="rounded-full bg-gray-300 w-10 h-10 flex-shrink-0"></div>
-                            </div>
-                        </div>
-
-                        <!-- Input field for sending messages -->
-                        <div class="flex items-center space-x-2 mt-4">
-                            <div class="text-gray-800 dark:text-gray-10 w-full">
-                                <input type="text" id="message" placeholder="Type your message" class="w-full" />
-                            </div>
-                            <button class="px-4 py-2 bg-blue-500 text-white rounded-md">Send</button>
-                        </div>
-                    </div>
+                <div class="flex flex-col space-y-4 h-96 overflow-y-auto">
+                    <!-- Chat messages -->
                 </div>
-            </div>
 
-            <!-- Left Side Menu -->
-            <div class="w-full sm:w-1/4 pl-4 pb-4">
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-4 left_row_users">
-                        <div class="text-lg font-semibold mb-4 text-gray-800 dark:text-gray-100">Chat</div>
-                        <!-- Search bar here -->
-                        <div class="flex items-center space-x-2 mb-2">
-                            <div class="text-gray-800 dark:text-gray-10 w-full">
-                                <input class="w-full" type="text" id="search" placeholder="Search users" />
-                            </div>
-                        </div>
-                        <!-- Display the users -->
-                        <div class="flex search-result items-start mb-2 flex-col">
-                        </div>
+                <!-- Input field for sending messages -->
+                <div class="flex items-center space-x-2 mt-4">
+                    <div class="text-gray-800 dark:text-gray-10 w-full">
+                        <input type="text" id="message" placeholder="Type your message" class="w-full" />
                     </div>
+                    <button class="px-4 py-2 bg-blue-500 text-white rounded-md">Send</button>
                 </div>
             </div>
         </div>
     </div>
-<<<<<<< Updated upstream
-=======
 
+    <!-- Left Side Menu -->
+    <div class="w-full sm:w-1/4 pl-4 pb-4">
+        <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-4 left_row_users">
+                <div class="text-lg font-semibold mb-4 text-gray-800 dark:text-gray-100">Chat</div>
+                <!-- Search bar here -->
+                <div class="flex items-center space-x-2 mb-2">
+                    <div class="text-gray-800 dark:text-gray-10 w-full">
+                        <input class="w-full" type="text" id="search" placeholder="Search users" />
+                    </div>
+                </div>
+                <!-- Display the users -->
+                <div class="flex search-result items-start mb-2 flex-col">
+                    <!-- Add click event listeners to open chat area -->
+                </div>
+            </div>
+        </div>
+    </div>
 
-
->>>>>>> Stashed changes
     <!-- Scripts -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script>
         $(document).ready(function() {
             var searchInput = $('#search');
             var userContainer = $('.search-result');
+            var chatArea = $('#chat-area');
+            var chatUsername = $('#chat-username');
             var searchBar = $('.search-bar');
             var previousResults = [];
 
@@ -111,19 +77,21 @@
 
                             if (users.length > 0) {
                                 users.forEach(function(user) {
-                                    var userElement = '<a href="/dashboard/' + user.id + '">' +
-                                        '<div class="flex items-center space-x-2 mb-2">' +
+                                    var userElement = $('<div class=" search-result-click flex items-center space-x-2 mb-2">' +
                                         '<div class="rounded-full bg-blue-500 w-10 h-10 flex-shrink-0"></div>' +
                                         '<div class="text-gray-800 dark:text-gray-100">' + user.name + '</div>' +
-                                        '</div>' +
-                                        '</a>';
+                                        '</div>');
+
+                                    userElement.on('click', function() {
+                                        openChatArea(user);
+                                    });
 
                                     userContainer.append(userElement);
 
                                     previousResults.push(user);
                                 });
                             } else {
-                                var noUsersElement = '<div class="text-gray-800 dark:text-gray-100">No users found</div>';
+                                var noUsersElement = $('<div class="text-gray-800 dark:text-gray-100">No users found</div>');
                                 userContainer.append(noUsersElement);
                             }
                         },
@@ -143,6 +111,12 @@
             searchInput.on('blur', function() {
                 searchBar.removeClass('active');
             });
+
+            function openChatArea(user) {
+                chatUsername.text(user.name);
+                chatArea.show();
+                // Additional logic to load chat messages and handle sending messages
+            }
         });
     </script>
 </x-app-layout>
